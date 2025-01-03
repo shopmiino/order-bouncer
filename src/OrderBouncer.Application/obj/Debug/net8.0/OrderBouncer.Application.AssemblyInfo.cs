fb074d48be07f4520433c55a54debcf2870c97fb
polatefekaya <polatsfekaya@gmail.com>
2025-01-03 15:12:55 +0300

    bu ne

--- src/OrderBouncer.Application/obj/Debug/net8.0/OrderBouncer.Application.AssemblyInfo.cs
+++ src/OrderBouncer.Application/obj/Debug/net8.0/OrderBouncer.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderBouncer.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+158d788ef365bfbc6a74feb244f81642b2e808ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3812636d247ebfd9baff101a23f393131d9d58cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderBouncer.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderBouncer.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
