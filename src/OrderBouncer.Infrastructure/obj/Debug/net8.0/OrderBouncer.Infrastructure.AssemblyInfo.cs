419e437826a30583d3a6b878602a09b4221033e7
Polat Efe Kaya <polatsfekaya@gmail.com>
2025-01-14 08:30:12 +0300

    ?

--- src/OrderBouncer.Infrastructure/obj/Debug/net8.0/OrderBouncer.Infrastructure.AssemblyInfo.cs
+++ src/OrderBouncer.Infrastructure/obj/Debug/net8.0/OrderBouncer.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderBouncer.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d338ad9688f2c0c4eb6323101822ac9e1fd2754")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d638a27693b61dfca8f1d54da1ac9c408ae86e26")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderBouncer.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderBouncer.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/SharedKernel/obj/Debug/net8.0/SharedKernel.AssemblyInfo.cs
+++ src/SharedKernel/obj/Debug/net8.0/SharedKernel.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SharedKernel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+094f8d23299df340e8efceab48b98833674a0b96")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a8b0b0057c9089a6f0135609d304a63463149c4")]
->>>>>>> feature
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d638a27693b61dfca8f1d54da1ac9c408ae86e26")]
 [assembly: System.Reflection.AssemblyProductAttribute("SharedKernel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SharedKernel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
