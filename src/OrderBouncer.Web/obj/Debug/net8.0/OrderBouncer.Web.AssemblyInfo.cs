2fe78bea5dda800f3db842ca9c72cd6f97ae0d76
polatefekaya <polatsfekaya@gmail.com>
2025-01-16 23:37:51 +0300

    wtf is this obj directory

--- src/OrderBouncer.Web/obj/Debug/net8.0/OrderBouncer.Web.AssemblyInfo.cs
+++ src/OrderBouncer.Web/obj/Debug/net8.0/OrderBouncer.Web.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderBouncer.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebf8c3189ec03e945dda5c917404381bc697e898")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e640b3a931070729586dec49fd0d893fda937f7f")]
+>>>>>>> cfc43dc (wtf is this obj directory)
 [assembly: System.Reflection.AssemblyProductAttribute("OrderBouncer.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderBouncer.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
