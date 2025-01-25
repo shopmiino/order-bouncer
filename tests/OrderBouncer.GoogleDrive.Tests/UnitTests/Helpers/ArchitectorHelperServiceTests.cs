029f61b7e9d0f87ff066ec34c0f4cb3450d88268
polatefekaya <polatsfekaya@gmail.com>
2025-01-25 04:00:15 +0300

    little test edit

--- tests/OrderBouncer.GoogleDrive.Tests/UnitTests/Helpers/ArchitectorHelperServiceTests.cs
+++ tests/OrderBouncer.GoogleDrive.Tests/UnitTests/Helpers/ArchitectorHelperServiceTests.cs
@@ -1,4 +1,9 @@
 using System;
+using AutoFixture;
+using OrderBouncer.Domain.DTOs.Base;
+using OrderBouncer.GoogleDrive.Interfaces;
+using OrderBouncer.GoogleDrive.Interfaces.Helpers;
+using OrderBouncer.GoogleDrive.Interfaces.UseCases;
 using OrderBouncer.GoogleDrive.Tests.Fixtures;
 
 namespace OrderBouncer.GoogleDrive.Tests.UnitTests.Helpers;
@@ -9,13 +14,19 @@ public class ArchitectorHelperServiceTests
     public ArchitectorHelperServiceTests(){
         _fixture = new();
     }
-    
+
     [Fact]
     public void TestName()
     {
         // Arrange
-    
+        var repo = _fixture.Fixture.Create<IGoogleDriveRepository>();
+        var namingHelper = _fixture.Fixture.Create<INamingHelperService>();
+        var manyToMany = _fixture.Fixture.Create<IManyToManyUseCase<BaseDto>>();
+        var manyToOne = _fixture.Fixture.Create<IManyToOneUseCase<BaseDto>>();
+        var oneToMany = _fixture.Fixture.Create<IOneToManyUseCase<BaseDto>>();
         // Act
+
+        
     
         // Assert
     }
