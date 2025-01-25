e833ec012d7dc54c9140e80db1cc09a64e4cd0ed
polatefekaya <polatsfekaya@gmail.com>
2025-01-25 03:49:57 +0300

    minor usecases test architecture changes

---
+++ tests/OrderBouncer.GoogleDrive.Tests/Customizations/UseCases/UseCaseCustomization.cs
@@ -0,0 +1,30 @@
+using System;
+using AutoFixture;
+using Bogus;
+using Moq;
+using OrderBouncer.Domain.DTOs.Base;
+using OrderBouncer.GoogleDrive.Constants;
+using OrderBouncer.GoogleDrive.Interfaces.UseCases;
+
+namespace OrderBouncer.GoogleDrive.Tests.Customizations.UseCases;
+
+public class UseCaseCustomization : ICustomization
+{
+    public void Customize(IFixture fixture)
+    {
+        Faker faker = new();
+
+        var manyToManyMock = fixture.Freeze<Mock<IManyToManyUseCase<BaseDto>>>();
+        var manyToOneMock = fixture.Freeze<Mock<IManyToOneUseCase<BaseDto>>>();
+        var oneToManyMock = fixture.Freeze<Mock<IOneToManyUseCase<BaseDto>>>();
+
+        manyToManyMock.Setup(x => x.ExecuteAsync(It.IsAny<FolderNamesEnum>(), It.IsAny<ICollection<BaseDto>>(), It.IsAny<List<string>>(), It.IsAny<CreationModes>()))
+            .ReturnsAsync((int count) => [.. Enumerable.Range(0, count).Select( _ => faker.Random.Guid().ToString())]);
+
+        manyToOneMock.Setup(x => x.ExecuteAsync(It.IsAny<FolderNamesEnum>(), It.IsAny<ICollection<BaseDto>>(), It.IsAny<string>(), It.IsAny<CreationModes>()))
+            .ReturnsAsync((int count) => [.. Enumerable.Range(0, count).Select( _ => faker.Random.Guid().ToString())]);
+
+        oneToManyMock.Setup(x => x.ExecuteAsync(It.IsAny<FolderNamesEnum>(), It.IsAny<BaseDto>(), It.IsAny<ICollection<string>>(), It.IsAny<CreationModes>()))
+            .ReturnsAsync((int count) => [.. Enumerable.Range(0, count).Select( _ => faker.Random.Guid().ToString())]);
+    }
+}
