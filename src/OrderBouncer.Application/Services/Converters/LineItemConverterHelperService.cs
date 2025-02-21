386eed8fa9e0511a1a3e2698021c44f113680b2b
Polat Efe Kaya <polatsfekaya@gmail.com>
2025-02-22 02:15:36 +0300

    logging added for various classes

--- src/OrderBouncer.Application/Services/Converters/LineItemConverterHelperService.cs
+++ src/OrderBouncer.Application/Services/Converters/LineItemConverterHelperService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Concurrent;
+using Microsoft.Extensions.Logging;
 using OrderBouncer.Application.DTOs;
 using OrderBouncer.Application.Interfaces.Converters;
 using OrderBouncer.Application.Interfaces.HttpClients;
@@ -9,25 +10,33 @@ namespace OrderBouncer.Application.Services.Converters;
 public class LineItemConverterHelperService : ILineItemConverterHelperService
 {
     private readonly IImageSaverService _imageSaver;
-    public LineItemConverterHelperService(IImageSaverService imageSaver)
+    private readonly ILogger<LineItemConverterHelperService> _logger;
+    public LineItemConverterHelperService(IImageSaverService imageSaver, ILogger<LineItemConverterHelperService> logger)
     {
         _imageSaver = imageSaver;
+        _logger = logger;
     }
 
     public async Task<ICollection<string>> BatchImageSaveAndAdd(NoteAttribute[] props, ICollection<string> imagePaths)
     {
+        _logger.LogInformation("BarchImageSaveAndAdd is starting with imagePath count: {0}", imagePaths.Count());
         var tempList = imagePaths.ToList();
+
 
         var tasks = props.Select(async p => {
                 string extension = p.Value.Split(".").Last();
+                _logger.LogDebug("Selected extension for {0}, {1}, is: {2}", p.Value, p.Name, extension);
+
                 string path = await _imageSaver.Save(p.Value, p.Name, extension);
                 return path;
         });
         
+        _logger.LogDebug("{0} tasks are grouped and starting to run in parallel with Task.WhenAll", tasks.Count());
         string[] results = await Task.WhenAll(tasks);
 
         tempList.AddRange(results);
 
+        _logger.LogDebug("Results added to tempImagePaths, current path count is {0}. Returning the tempPaths.", tempList.Count());
         return tempList;
     }
 }
