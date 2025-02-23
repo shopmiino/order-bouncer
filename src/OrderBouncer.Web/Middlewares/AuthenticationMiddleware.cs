e465adf29862a920c7460f2e8a055d02bfee56a3
Polat Efe Kaya <polatsfekaya@gmail.com>
2025-02-24 02:23:40 +0300

    authentication added, rest parameter is configured

---
+++ src/OrderBouncer.Web/Middlewares/AuthenticationMiddleware.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace OrderBouncer.Web.Middlewares;
+
+public class AuthenticationMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<AuthenticationMiddleware> _logger;
+
+    private const string SHOPIFY_AUTH_HEADER_NAME = "X-Shopify-Hmac-SHA256";
+    public AuthenticationMiddleware(RequestDelegate next, ILogger<AuthenticationMiddleware> logger){
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context, IConfiguration configuration){
+        _logger.LogInformation("AuthenticationMiddleware is starting");
+
+        string? shopifySecret = configuration["Shopify:WebhookSecret"];
+        if(shopifySecret is null) throw new ArgumentNullException("ShopifySecret is null, can not retrieve from appsettings");
+
+        string hmacHeader = context.Request.Headers[SHOPIFY_AUTH_HEADER_NAME].ToString();
+
+        if(string.IsNullOrEmpty(hmacHeader)){
+            _logger.LogError("Request missing Shopify HMAC Header");
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            await context.Response.WriteAsync("Unauthorized, missing Shopify HMAC Header");
+            return;
+        }
+
+        _logger.LogDebug("HMAC successfully arrived from request");
+        _logger.LogTrace("Arrived HMAC: {0}", hmacHeader);
+
+        context.Request.EnableBuffering();
+        using StreamReader reader = new StreamReader(context.Request.Body, encoding: Encoding.UTF8, leaveOpen: true);
+        string body = await reader.ReadToEndAsync();
+        context.Request.Body.Position = 0;
+
+        using HMACSHA256 hmac = new HMACSHA256(Encoding.UTF8.GetBytes(shopifySecret));
+        byte[] computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(body));
+        string hash = Convert.ToBase64String(computedHash);
+
+        if(hash != hmacHeader){
+            _logger.LogError("Invalid Shopify webhook secret");
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            await context.Response.WriteAsync("Unauthorized, Invalid Shopify webhook secret");
+            return;
+        }
+
+        _logger.LogInformation("Shopify webhook successfully authenticated");
+
+        await _next(context);
+    }
+}
